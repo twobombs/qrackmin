ea71661381407379b26be2f54b73d2c146702b40
Aryan Blaauw <ablaauw@gmx.net>
2025-04-13 16:50:04 +0200

    Update run-qbdd-ising-loop.sh

--- run-scripts/run-qbdd-ising-loop.sh
+++ run-scripts/run-qbdd-ising-loop.sh
@@ -10,7 +10,7 @@ for ((i=4; i<=56; i++)); do
   for ((j=4; j<=i; j++)); do
     echo "Running i=$i, j=$j"
     # Execute python script in the background
-    python3 ising_qbdd_fixes.py "$j" "$i" 2>>errors_ising_qbdd_fixes_$(echo $i)_$(echo $j).txt 1>>ising_qbdd_fixes_$(echo $i)_$(echo $j).txt &
+    python3 run-ising_qbdd_fixes.py "$j" "$i" 2>>errors_ising_qbdd_fixes_$(echo $i)_$(echo $j).txt 1>>ising_qbdd_fixes_$(echo $i)_$(echo $j).txt &
     # Store the PID of the last backgrounded process
     pids+=($!)
 
