c05d25f6bac71dc0d73792880e3486c2c7181f2c
Aryan Blaauw <ablaauw@gmx.net>
2025-04-13 16:49:37 +0200

    Update run-qbdd-ising-loop.sh

--- run-scripts/run-qbdd-ising-loop.sh
+++ run-scripts/run-qbdd-ising-loop.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cd /pyqrack-examples/ising
-
-# Create or clear the error log file at the start
-> errors_ising_qbdd_fixes.txt
+# cd /pyqrack-examples/ising
 
 for ((i=4; i<=56; i++)); do
   # Array to store PIDs of background jobs for the current 'i'
@@ -13,7 +10,7 @@ for ((i=4; i<=56; i++)); do
   for ((j=4; j<=i; j++)); do
     echo "Running i=$i, j=$j"
     # Execute python script in the background
-    python3 ising_qbdd_fixes.py "$j" "$i" 2>>errors_ising_qbdd_fixes.txt 1>>ising_qbdd_fixes_$(echo $i)_$(echo $j).txt &
+    python3 ising_qbdd_fixes.py "$j" "$i" 2>>errors_ising_qbdd_fixes_$(echo $i)_$(echo $j).txt 1>>ising_qbdd_fixes_$(echo $i)_$(echo $j).txt &
     # Store the PID of the last backgrounded process
     pids+=($!)
 
