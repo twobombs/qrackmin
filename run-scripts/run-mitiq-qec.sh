ecd25810780674bc706a7bfebe625694c4ad208d
Aryan Blaauw <ablaauw@gmx.net>
2025-05-17 17:25:12 +0200

    Create run-mitiq-qec.sh

---
+++ run-scripts/run-mitiq-qec.sh
@@ -0,0 +1,2 @@
+export QRACK_MAX_PAGING_QB=27 && export QRACK_QTENSORNETWORK_THRESHOLD_QB=1 && export QRACK_OCL_DEFAULT_DEVICE=0
+python3 /notebook/qrack/pyqrack-examples/qec/mitiq_repetition_code.py 36
